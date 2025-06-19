df8f1920136d09c23e96d414a76e07d6200f8a81
ArmanShaikh11 <arman.221439.it@mhssce.ac.in>
2025-06-19 23:21:33 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+<script type="module" src="/src/main.tsx"></script>
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
